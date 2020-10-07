be6811356749ea653678613089f10a2464570f7c
Leonardo Sampaio Valadão Moraes <42561943+Leonardo-Moraes@users.noreply.github.com>
2020-10-07 16:52:57 -0300

    Add files via upload

---
+++ palindromo.js
@@ -0,0 +1,11 @@
+
+const palindromo = 'Ana';
+        if (palindromo.toLowerCase[0] == palindromo.toLowerCase[2] && palindromo.toLowerCase[2] == palindromo.toLowerCase[0] && palindromo.toLowerCase[1] == palindromo.toLowerCase[1]) {
+           
+            console.log('isto é um palindromo')
+        }
+        else {
+            console.log('isto não é um palindromo')
+        }
+
+
